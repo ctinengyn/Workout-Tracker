ec5f2e63d80ffe01b8e65e52dcf807d6caca8123
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-08 20:19:15 -0700

    Connects to MongoDB

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 // Routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
