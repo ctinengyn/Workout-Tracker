6a9bd3e0d77a5e928f3f52772e39c6cf42408586
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-08 20:18:01 -0700

    Comments and routes added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
 
