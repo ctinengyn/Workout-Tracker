d57a6f9408164405ff01321d2c665570bf526422
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-10 00:27:40 -0700

    Changed connection for MongoDB

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connects to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
